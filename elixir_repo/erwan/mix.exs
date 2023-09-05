d2668a113166b2b1b24d7fefbf7ee80a736cad32
Olivier DEPREZ <olivier.deprez@gmail.com>
2023-09-05 10:00:53 +0200

    Adding VegaLite to JS

--- elixir_repo/erwan/mix.exs
+++ elixir_repo/erwan/mix.exs
@@ -50,7 +50,8 @@ defmodule Erwan.MixProject do
       {:swoosh, "~> 1.3"},
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"}
+      {:telemetry_poller, "~> 1.0"},
+      {:vega_lite, "~> 0.1"}
     ]
   end
 
